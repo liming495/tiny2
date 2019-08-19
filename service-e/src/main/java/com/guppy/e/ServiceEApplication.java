c689eb6653d89e111b8833b1c3ce92a9ac1ed416
wop129@tom.com <wop129@tom.com>
2019-08-19 14:38:59 +0800

    add e model

---
+++ service-e/src/main/java/com/guppy/e/ServiceEApplication.java
@@ -0,0 +1,13 @@
+package com.guppy.e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEApplication.class, args);
+    }
+
+}
