36494e04a4802946a028223fe273c7aa6106506e
wop129@tom.com <wop129@tom.com>
2019-08-20 23:02:32 +0800

    debug

--- auth-service/src/main/java/com/guppy/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/guppy/auth/AuthServiceApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableDiscoveryClient
 public class AuthServiceApplication {
 
