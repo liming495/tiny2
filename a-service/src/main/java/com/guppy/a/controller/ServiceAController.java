764e64f326d0ba6f17b0f41225bd11c0c2a444fb
wop129@tom.com <wop129@tom.com>
2019-08-22 14:35:50 +0800

    modify

--- a-service/src/main/java/com/guppy/a/controller/ServiceAController.java
+++ a-service/src/main/java/com/guppy/a/controller/ServiceAController.java
@@ -2,8 +2,10 @@ package com.guppy.a.controller;
 
 import com.guppy.a.client.ServiceBClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
@@ -25,15 +27,16 @@ public class ServiceAController {
     private String name;
 
     @Autowired
-    EurekaDiscoveryClient discoveryClient;
-    @Autowired
-    ServiceBClient serviceBClient;
+    private LoadBalancerClient client;
     @Autowired
     Registration registration;
+    @Qualifier("b-service")
+    @Autowired
+    ServiceBClient serviceBClient;
 
     @GetMapping(value = "/")
     public String printServiceA(){
-        ServiceInstance serviceInstance = serviceInstance();
+        ServiceInstance serviceInstance = client.choose(registration.getServiceId());
         return serviceInstance.getServiceId()
                 + " (" + serviceInstance.getHost()
                 + ":"
@@ -46,15 +49,4 @@ public class ServiceAController {
 
     @GetMapping(path = "/current")
     public Principal getCurrentAccount(Principal principal){  return principal; }
-
-    public ServiceInstance serviceInstance() {
-        List<ServiceInstance> list = discoveryClient.getInstances(registration.getServiceId());
-        if (list != null && list.size() > 0) {
-            for(ServiceInstance itm : list){
-                if(itm.getPort() == 2001)
-                    return itm;
-            }
-        }
-        return null;
-    }
 }
