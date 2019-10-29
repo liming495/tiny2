762060048616449ee7ea08cba7188bfeee07ab59
wop129@tom.com <wop129@tom.com>
2019-10-29 16:02:07 +0800

    modify

--- b-service/src/main/java/com/guppy/b/controller/ServiceBController.java
+++ b-service/src/main/java/com/guppy/b/controller/ServiceBController.java
@@ -1,16 +1,15 @@
 package com.guppy.b.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * 服务B控制器
@@ -20,6 +19,8 @@ import java.util.List;
 @RefreshScope
 @RestController
 public class ServiceBController {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceBController.class);
+
     @Autowired
     private LoadBalancerClient client;
     @Autowired
@@ -30,14 +31,19 @@ public class ServiceBController {
 
     @GetMapping(value = "/")
     public String printServiceB(){
-        ServiceInstance serviceInstance = client.choose(registration.getServiceId());
-        return serviceInstance.getServiceId()
-                + " ("
-                + serviceInstance.getHost()
-                + ":"
-                + serviceInstance.getPort()
-                + ")"
-                + "===>Say "
-                + msg;
+        try {
+            ServiceInstance serviceInstance = client.choose(registration.getServiceId());
+            return serviceInstance.getServiceId()
+                    + " ("
+                    + serviceInstance.getHost()
+                    + ":"
+                    + serviceInstance.getPort()
+                    + ")"
+                    + "===>Say "
+                    + msg;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw e;
+        }
     }
 }
